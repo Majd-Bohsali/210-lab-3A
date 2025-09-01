864682e4dfa7ab4adcf6d2cbb9dea32e131eb3da
Majd-Bohsali <118876575+Majd-Bohsali@users.noreply.github.com>
2025-09-01 15:32:12 -0700

    Can successfully read restaurant name

--- 210-lab-3A.cpp
+++ 210-lab-3A.cpp
@@ -2,8 +2,10 @@
 // IDE used: Visual Studio Code
 #include <iostream>
 #include <vector> 
+#include <string>
 using namespace std; 
 
+// defines a struct datatype with attributes for the name, address, carryingCapacity, avgRating, and openHours
 struct Restaurant { 
     string name; 
     string address; 
@@ -12,7 +14,16 @@ struct Restaurant {
     string openHours;   
 };
 
+Restaurant returnRestarantStruct() { 
+    Restaurant tempRestaurant; 
+    cout << "Enter the Restaurant's name: ";
+    getline(cin, tempRestaurant.name);
+    cout << tempRestaurant.name; 
+    return tempRestaurant; 
+}
+
 int main() { 
     cout << "Hello World" << endl; 
+    returnRestarantStruct();
     return 0; 
 }

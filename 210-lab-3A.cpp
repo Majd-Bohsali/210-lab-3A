ae628ffb03667b56fb53855500f2d957a9e572a4
Majd-Bohsali <118876575+Majd-Bohsali@users.noreply.github.com>
2025-09-01 15:26:05 -0700

    Created struct

--- 210-lab-3A.cpp
+++ 210-lab-3A.cpp
@@ -1,7 +1,16 @@
 // COMSC-210 | Lab 3A | Majd Bohsali
 // IDE used: Visual Studio Code
 #include <iostream>
+#include <vector> 
 using namespace std; 
+
+struct Restaurant { 
+    string name; 
+    string address; 
+    int carryingCapacity; 
+    float avgRating; 
+    string openHours;   
+};
 
 int main() { 
     cout << "Hello World" << endl; 

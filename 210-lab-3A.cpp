c7ddff58379ef09d7733855f922886cb835c95c3
Majd-Bohsali <118876575+Majd-Bohsali@users.noreply.github.com>
2025-09-01 15:22:06 -0700

    Initial commit

---
+++ 210-lab-3A.cpp
@@ -0,0 +1,9 @@
+// COMSC-210 | Lab 3A | Majd Bohsali
+// IDE used: Visual Studio Code
+#include <iostream>
+using namespace std; 
+
+int main() { 
+    cout << "Hello World" << endl; 
+    return 0; 
+}
